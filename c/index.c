38b795cf09ddf77925b8041cabbaae3dfaeeb920
DeboraZandonai <debohzandonaii@gmail.com>
2020-09-25 14:33:05 -0300

    commit initial

---
+++ c/index.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Olá Mundo!\n");
+
+    system('pause')
+
+        return 0;
+}
+
+int main()
+{
+
+    int n1, n2;
+
+    printf("Digite o primeiro número:")
+        scanf_s("%d", &n1)
+            printf("Digite o segundo número:")
+                scanf_s("%d", &n2)
+                    printf("Os números digitados são: %d e %d\n", n1, n2)
+
+                        system('pause')
+
+                            return 0;
+}
