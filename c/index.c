7ee3e525b89177958ddf82e76e14889f23fa8c78
DeboraZandonai <debohzandonaii@gmail.com>
2020-09-26 13:41:45 -0300

    update

--- c/index.c
+++ c/index.c
@@ -25,3 +25,34 @@ int main()
 
                             return 0;
 }
+
+int main()
+{
+    int option;
+
+    printf("Digite um número entre 1 e 7:")
+        scanf_s("%d", &option)
+
+            switch (option)
+    {
+    case 1:
+        printf("O dia escolhido foi Domingo.\n\n") break;
+    case 2:
+        printf("O dia escolhido foi Segunda-Feira.\n\n") break;
+    case 3:
+        printf("O dia escolhido foi Terça-Feira.\n\n") break;
+    case 4:
+        printf("O dia escolhido foi Quarta-Feira.\n\n") break;
+    case 5:
+        printf("O dia escolhido foi Quinta-Feira.\n\n") break;
+    case 6:
+        printf("O dia escolhido foi Sexta-Feira.\n\n") break;
+    case 7:
+        printf("O dia escolhido foi Sábado.\n\n") break;
+
+    default:
+        printf("Não existe dia da semana para este número!") break;
+    }
+
+    system('pause') return 0;
+}
